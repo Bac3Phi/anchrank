dc5b225ef8c0324b69d0ef01cbf603349f4cbbd0
Bac3Phi <gkztest1@gmail.com>
2020-06-21 00:42:28 +0700

    Init

---
+++ components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title has-text-grey">
+        {{ title }}
+      </p>
+    </header>
+    <div class="card-content">
+      <div class="content has-text-centered">
+        <img :src="icon_uri" :style="{ width: 100 }">
+      </div>
+    </div>
+    <footer class="card-footer">
+      <div class="card-footer-item">
+        <slot />
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    icon_uri: {
+      type: String
+    }
+  }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="main">
+    <b-navbar>
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img
+            src="~assets/Blathers.png"
+            alt="Lightweight UI components for Vue.js based on Bulma"
+          />
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item v-for="(item, key) of items" :key="key">
+          <nuxt-link :to="item.to" exact-active-class="is-active">
+            <b-icon :icon="item.icon" /> {{ item.title }}
+          </nuxt-link>
+        </b-navbar-item>
+
+      </template>
+
+      <template slot="end">
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">
+              Log in
+            </a>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+
+    <section class="main-content columns">
+      <div class="container">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: "Home",
+          icon: "home",
+          to: { name: "index" }
+        },
+        {
+          title: "Chill",
+          icon: "coffee",
+          to: { name: "chill" }
+        }
+      ]
+    };
+  }
+};
+</script>
+
